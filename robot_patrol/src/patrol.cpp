5e79e1d490760380fd8e40c7f860dba7631a8936
Gokhulraj6200 <gokhulraj6200@gmail.com>
2024-03-25 20:55:28 +0000

    Improving accuracy to the bot

--- robot_patrol/src/patrol.cpp
+++ robot_patrol/src/patrol.cpp
@@ -2,6 +2,7 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
+#include <cmath>
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -18,41 +19,70 @@ public:
 
     timer_ = this->create_wall_timer(
             500ms, std::bind(&Patrol::timer_callback, this));
+    
+    //pi
+    pi_= std::atan2(0 , -1);
  
  }
 
 private:
     
     void laserCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+        int max_index = 0; 
+        float max_obj = 0;
         int size = static_cast<int>(msg->ranges.size());
         float front = msg->ranges[330];
         float left = msg->ranges[165];
         float right = msg->ranges[495];
+        float max_range = msg->range_max;
+        float direction_;
         RCLCPP_INFO(this->get_logger(), "The laser scans array has a size of: %i", size);
         RCLCPP_INFO(this->get_logger(), "The front laser is: %.2f", front);
         RCLCPP_INFO(this->get_logger(), "The left laser is: %.2f", left);
         RCLCPP_INFO(this->get_logger(), "The right laser is: %.2f", right);
-            auto velocity = geometry_msgs::msg::Twist();
-            if (front > stop) {
+
+        for(int i = 0; i < size; i++){
+                if (msg->ranges[i] < max_range && msg->ranges[i] > max_obj)
+                {
+                    max_obj = msg->ranges[i];
+                    max_index = i;
+                }
+        }
+           direction_ = max_index * 2 * pi_ / size;
+           RCLCPP_INFO(this->get_logger(), "The max object is: %.2f and index is: %i", max_obj , max_index);
+           auto velocity = geometry_msgs::msg::Twist();
+            if (front >= 0.5) {
                 RCLCPP_INFO(this->get_logger(), "robot moves forward");
                 velocity.linear.x = -0.1;
                 publisher_->publish(velocity);
                 
             }
             else {
-                RCLCPP_INFO(this->get_logger(), "robot rotates");
+
+            if (max_index > 330){
+                RCLCPP_INFO(this->get_logger(), "robot rotates counterclockwise");
                 velocity.linear.x = 0;
-                velocity.angular.z = 0.5;
+                velocity.angular.z = direction_/2;
                 publisher_->publish(velocity);
             }
-
+            else {
+            {
+                RCLCPP_INFO(this->get_logger(), "robot rotates clockwise");
+                velocity.linear.x = 0;
+                velocity.angular.z = -direction_/2;
+                publisher_->publish(velocity);
+            }
+            }
+            }
+            
 
      }
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-  float stop= 0.7;
+  //float stop= 0.7;
+  float pi_;
 
   void timer_callback() {}
 };
